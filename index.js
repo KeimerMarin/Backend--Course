39f74bce57811d49d167a6ce74e43a9f212e4238
keimer marin <dark_keimer@hotmail.com>
2022-06-01 11:52:54 -0400

    Desafio N°2

--- index.js
+++ index.js
@@ -1,35 +1,89 @@
-class Usuario{
-    constructor(nombreUsuario, apellidoUsuario, mascota, libros){
-        this.nombreUsuario = nombreUsuario
-        this.apellidoUsuario = apellidoUsuario
-        this.mascota= [];
-        this.libros = []
-        
-    }
-    getFullName ()  {
-        return `${this.nombreUsuario} ${this.apellidoUsuario}`     
-    }   
-    addMascota () { 
-        this.mascota.push('perro', 'gato', 'loro')
+const fs = require("fs");
 
+class Contenedor {
+  constructor(archivo) {
+    this.file = archivo 
+  }
+
+// Metodo Save 
+
+  async save(objeto) {    
+    let data = await fs.promises.readFile(`./${this.file}`, 'utf-8')
+        if(!data) {
+            let id = JSON.parse(await fs.promises.readFile('./identifier.txt', 'utf-8'))
+            let maxID = Math.max(...id)
+            objeto.id = maxID + 1;
+            id = [...id, objeto.id]
+            await fs.promises.writeFile(`./identifier.txt`, JSON.stringify(id));            
+            await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(objeto))}
+
+            else {
+    let id = JSON.parse(await fs.promises.readFile('./identifier.txt', 'utf-8'))
+    let maxID = Math.max(...id)
+    objeto.id = maxID + 1;
+    id = [...id, objeto.id]
+    await fs.promises.writeFile(`./identifier.txt`, JSON.stringify(id))
+
+    let productos = JSON.parse(
+      await fs.promises.readFile('./products.txt', 'utf-8')
+    );
+
+    productos.push(objeto);
+    await fs.promises.writeFile(`./${this.file}`, JSON.stringify(productos))
+    console.log('Producto agregado con exito ID', objeto.id)
+  }}
+
+// Metodo getById
+
+  async getById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./products.txt", "utf-8")
+    );
+    let objeto = productos.find((prod) => prod.id == id)
+    console.log(objeto ? objeto : "ID no encontrado")
+  }
+
+// Metodo getAll
+
+  async getAll() {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./products.txt", "utf-8")
+    );
+    console.log(productos);
+  }
+
+// Metodo deleteById
+
+  async deleteById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./products.txt", "utf-8")
+    );
+    if (productos.some((prod) => prod.id == id)) {
+      let newProductos = productos.filter((prod) => prod.id != id)
+      await fs.promises.writeFile(
+        `./${this.file}`,
+        JSON.stringify(newProductos)
+      );
+      console.log('producto eliminado correctamente')
+    } else {
+      console.log('no existe producto asociado al id')
     }
-    countMascota () {
-        return `${this.mascota.length}`  
+  }
+
+//Metodo deteleAll
+
+  async deleteAll() {
+    try {
+    await fs.promises.writeFile(`./${this.file}`, '[]')
+    console.log('Se han eliminado todos los productos')
+    }catch(error) {
+        console.log(error)
     }
-    addBook () {
-        this.libros.push({nombre: 'EL principe azul', autor: 'Desconocido'}, {nombre: 'el principe rojo', autor: 'Sin Nombre'})
-    }
-    getBookNames () {
-        return this.libros.map(item => item.nombre);
-    }
+  }
 }
 
-const persona1 = new Usuario("Keimer", "Marin")
-
-console.log(persona1)
-console.log (persona1.getFullName())
-console.log(persona1.addMascota())
-console.log(persona1.countMascota())
-console.log(persona1.addBook())
-console.log(persona1) 
-console.log(persona1.getBookNames())
+const arrayProducto = new Contenedor('products.txt').save({nombre: 'productoPrueba', precio: 9990, thumbnail: 'https://www.zoominformatica.com/img-datos/AHD301A_0.jpg'})
+// new Contenedor('products.txt').getById(8)
+// new Contenedor('products.txt').getAll()
+// new Contenedor('products.txt').deleteById(12) 
+// new Contenedor('products.txt').deleteAll()
